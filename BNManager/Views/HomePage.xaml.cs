e8d30519ea4887814ef0dd02460a81f609609792
minisbett <39670899+minisbett@users.noreply.github.com>
2024-05-27 18:48:47 +0200

    refactor homepage

--- BNManager/Views/HomePage.xaml.cs
+++ BNManager/Views/HomePage.xaml.cs
@@ -1,4 +1,7 @@
+using BNManager.Services;
+using BNManager.ViewModels;
 using Microsoft.UI.Xaml.Controls;
+using System;
 
 namespace BNManager.Views;
 
@@ -7,5 +10,19 @@ public sealed partial class HomePage : Page
   public HomePage()
   {
     InitializeComponent();
+  }
+
+  /// <summary>
+  /// Opens a dialog to create a new project, creating it if the user confirms.
+  /// </summary>
+  private async void CreateProject_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+  {
+    CreateProjectDialog cpd = new CreateProjectDialog()
+    {
+      XamlRoot = MainWindow.XamlRoot
+    };
+
+    if (await cpd.ShowAsync() == ContentDialogResult.Primary)
+      ProjectService.Create((cpd.DataContext as CreateProjectDialogViewModel).BeatmapSet);
   }
 }
